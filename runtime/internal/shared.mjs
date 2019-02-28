027d96453a002a2ecb37ac612c6ea6cb4be4b5bd
Thomas Ghysels <info@thomasg.be>
2019-02-28 03:13:40 +0100

    Avoid leaking session and page data

--- runtime/internal/shared.mjs
+++ runtime/internal/shared.mjs
@@ -1,11 +1,25 @@
 import { writable } from 'svelte/store';
+import { noop } from 'svelte/internal';
 
 export const stores = {
-	session: writable(false),
+	session: process.browser ? writable(false) : frozen(false),
 	preloading: writable(false),
-	page: writable(null)
+	page: process.browser ? writable(null) : frozen(null)
 };
 
 export const CONTEXT_KEY = {};
 
 export const preload = () => ({});
+
+function frozen (value) {
+	function reset(newValue) {
+		value = newValue;
+	}
+
+	function subscribe(run) {
+		run(value);
+		return noop;
+	}
+
+	return { reset, subscribe };
+}
